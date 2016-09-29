9147aaa69a528d14f2e662e3fbe2c6210b77db7d
philsstein <phil.s.stein@gmail.com>
2016-09-29 09:52:38 -0400

    added "post sticky" and "post unsticky" bg3po mail comamnds.

--- bin/mail-autoreply.py
+++ bin/mail-autoreply.py
@@ -9,7 +9,8 @@ import sys
 import re
 from bg3po_oauth import login
 
-SUBREDDIT='boardgames'
+#SUBREDDIT='boardgames'
+SUBREDDIT='bg_tech'
 AUTOREPLY_MSG = """\
 You have PM'ed /u/bg3po.  This user is a bot account for doing administrative
 tasks for the moderators of /r/boardgames. No human checks this account.
@@ -45,9 +46,35 @@ def set_ks_link(r, msg):
     new_sb = re.sub(r'\[Kickstarter Roundup ([0-9/]+)\]\(\/[a-z0-9]+\)', new_ks_link, sb, 1)
     sr.update_settings(description=new_sb)
 
+def to_stick_or_not_to_stick_that_is_the_question(r, msg, sticky):
+    mode = 'sticky' if sticky else 'unsticky'
+    print('Setting post to {}'.format(mode))
+    sr = r.get_subreddit(SUBREDDIT)
+    
+    url = msg.body
+    print('getting submission: {}'.format(url))
+    sub = r.get_submission(url)
+    if not sub:
+        print('Error finding post with given URL: {}'.format(url))
+        return
+
+    if not sticky:
+        sub.unsticky()
+    else:
+        sub.sticky()
+
+def set_unsticky(r, msg):
+    '''Set the given thread to be sticky.'''
+    to_stick_or_not_to_stick_that_is_the_question(r, msg, sticky=False)
+
+def set_sticky(r, msg):
+    '''Set the given thread to be sticky.'''
+    to_stick_or_not_to_stick_that_is_the_question(r, msg, sticky=True)
+
 # all /r/bg mods can execute any command
 mods = [
-    'phil_s_stein'
+    'phil_s_stein',
+    'Epsilon_balls'
 ]
 
 # specific commands may be executable by non-mods. 
@@ -55,6 +82,14 @@ cmds = {
     'setksru': {
         'allowed': ['Zelbinian', 'whygook', 'anahuac-a-mole'] + mods,
         'cmd': set_ks_link
+    },
+    'sticky post': {
+        'allowed': ['Lazarus1209'] + mods,
+        'cmd': set_sticky
+    },
+    'unsticky post': {
+        'allowed': ['Lazarus1209'] + mods,
+        'cmd': set_unsticky
     }
 }
 
